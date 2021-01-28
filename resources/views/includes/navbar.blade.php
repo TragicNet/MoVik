bcb28a9d853b97f4bb1c096b248113da42431b2b
MaxPathak <maxpathakx377@gmail.com>
2021-01-28 10:32:28 +0530

    commit

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,6 @@
+@section('navbar')
+    <div class="navbar">
+        <a class="navitem logo" href="\home">MoVik</a>
+        <a class="navitem" href="\menu">Menu</a>
+    @show
+</div>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,16 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="theme-light">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MoVik</title>
+    <link rel="stylesheet" href="{{ mix('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Kalam' rel='stylesheet'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.themeButton').click(function() {
+                var html = document.documentElement;
+                $(html).toggleClass('theme-light');
+                $(html).toggleClass('theme-dark');
+            });
+        });
+
+    </script>
 </head>
 
 <body>
+
+    <header class="header"><a class="logo" href="\home">
+            <span class="title">MoVik</span>
+            <span class="tagline">Your Food is On the Way</span>
+        </a></header>
+
+    @include('includes.navbar')
+
     @yield('content')
 
     @include('includes.sidebar')
+
+    <button class="themeButton">Theme</button>
 
 </body>
 
