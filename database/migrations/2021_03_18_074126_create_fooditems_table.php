ca457e9a81e53b408c060af45053ed6c4315b37e
MaxPathak <67355216+MaxPathak@users.noreply.github.com>
2021-03-18 19:03:00 +0530

    commit

---
+++ database/migrations/2021_03_18_074126_create_fooditems_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFooditemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fooditems', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('menu_id');
+            $table->foreign('menu_id')->references('id')->on('users');
+            $table->string('name');
+            $table->string('img_path');
+            $table->string('description');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fooditems');
+    }
+}
